8696f3491e54c664a9d854ba5ac17a461b0a6772
Cory Fabian <Coryf65@users.noreply.github.com>
2023-03-23 15:43:20 -0500

    Update AuthController.cs
    adding checks for bad username or password
    we can also verify a hash

--- JWTAuth.API/Controllers/AuthController.cs
+++ JWTAuth.API/Controllers/AuthController.cs
@@ -21,12 +21,48 @@ public class AuthController : ControllerBase
         return Ok(user);
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UserDTO request)
+    {
+        // check if user exists
+        if (user.Username != request.Username)
+            return BadRequest("User not found.");
+        // verify the password is correct
+        if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            return BadRequest("username / password issue, try again.");
+
+
+        return Ok("TOKEN");
+    }
+
+    /// <summary>
+    /// Create a hash and salt for the given password
+    /// </summary>
+    /// <param name="password"></param>
+    /// <param name="passwordHash"></param>
+    /// <param name="passwordSalt"></param>
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
         using (var hmac = new HMACSHA512())
         {
             passwordSalt = hmac.Key;
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+    /// <summary>
+    /// Verify the password given if it matches the hash computed
+    /// </summary>
+    /// <param name="password"></param>
+    /// <param name="passwordHash"></param>
+    /// <param name="passwordSalt"></param>
+    /// <returns></returns>
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
         }
     }
 }
