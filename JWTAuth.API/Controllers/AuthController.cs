7bdb68574e8f4e0833bdbbc2b6eeac630b3547f2
Cory Fabian <Coryf65@users.noreply.github.com>
2023-03-23 15:14:43 -0500

    hashing and salting
    added a method to create the hash and salt for a user from the incoming password and username

---
+++ JWTAuth.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuth.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    public static User user = new User();
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDTO request)
+    {
+        CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.Username = request.Username;
+        user.PasswordSalt = passwordSalt;
+        user.PasswordHash = passwordHash;
+
+        return Ok(user);
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
---
+++ JWTAuth.API/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTAuth.API;
+
+/// <summary>
+/// User login and Registration
+/// </summary>
+public class UserDTO
+{
+    public string Username { get; set; }  = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
